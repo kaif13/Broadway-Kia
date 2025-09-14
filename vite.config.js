d5ff6405c007193101907556c8609cd9ce990fb6
Kaif13 <mohammaddkaif0602@gmail.com>
2025-09-14 21:32:43 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react()],
+  build: {
+    outDir: "dist", // ✅ default output folder
+  },
 });
